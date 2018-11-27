d097dc1d9a89af519736b5574912756c93b5f961
nwooldridge <nwooldridge1996@gmail.com>
2018-11-27 17:35:11 -0600

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include <cstdlib>
 
+using namespace std;
+
+int main() {
+        int passwordFile, maxPasswordLength;
+        cout << "Enter password file (2 or 3): ";
+        cin >> passwordFile;
+        cout << endl << "Enter max password length (3-8): ";
+        cin >> maxPasswordLength;
+        cout << passwordFile << " " << maxPasswordLength << endl;
+        
+  return 0;
+}
